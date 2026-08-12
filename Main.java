cf59b0a7b9ac21299d5566c05bfc7b0286b09be2
simonequeiroz <sqb1410@gmail.com>
2026-08-11 22:29:48 -0300

    Codigo java para a primeira pipeline da aula Devops

---
+++ Main.java
@@ -0,0 +1,10 @@
+// Cria uma classe publica chamada Main
+
+public class Main {
+    // Cria função principal da classe main
+
+    public static void main(String[] args) {
+        System.out.println("Olá, Devops !");
+        System.out.println("Pipeline executada com sucesso!");
+    }
+}
